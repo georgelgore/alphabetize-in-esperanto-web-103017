ebbed5f597e4fddc98157b2c558fb60e8799ffb9
George Gore <georgelgore@gmail.com>
2017-10-26 15:35:42 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  hash = {}
+
+  alphabet.chars.each_with_index do |letter, index|
+    hash[letter] = index
+  end
+
+  arr.sort_by{|string| string.chars.map{|el| hash[el]}}
+
+
+
 end
